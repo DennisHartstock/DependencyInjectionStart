efea56726e63a5d901cf70c48138a8c72e780c67
DennsHartstock <dennis.hartstock@web.de>
2022-09-21 08:23:35 +0200

    Dagger: inject viewModel

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.dependencyinjectionstart.example2.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.dependencyinjectionstart.R
 import com.example.dependencyinjectionstart.example2.ExampleApp
 import javax.inject.Inject
@@ -19,7 +20,18 @@ class MainActivity : AppCompatActivity() {
 //    }
 
     @Inject
-    lateinit var viewModel: ExampleViewModel
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val viewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[ExampleViewModel::class.java]
+    }
+
+    private val viewModel2 by lazy {
+        ViewModelProvider(this, viewModelFactory)[ExampleViewModel2::class.java]
+    }
+
+//    @Inject
+//    lateinit var viewModel: ExampleViewModel
 
     private val component by lazy {
         (application as ExampleApp).component
@@ -36,6 +48,7 @@ class MainActivity : AppCompatActivity() {
 //        Log.d("MainActivity", "${component.getApiService()}")
 //        Log.d("MainActivity", "${component.getApiService()}")
         viewModel.method()
+        viewModel2.method()
 //        val activity = Activity()
 //        activity.computer.toString()
 //        activity.keyboard.toString()
